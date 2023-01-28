21d8c68dff94565130ddb202098d0116c57f13fa
Thomas Schreiner <iteano.thom@hotmail.com>
2023-01-27 22:07:16 -0300

    feat: add footer component

---
+++ src/components/Footer/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledFooter = styled.footer`
+  background-color: var(--grey-4);
+  height: 34px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 12px;
+`;
