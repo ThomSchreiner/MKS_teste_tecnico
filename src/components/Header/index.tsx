89027d3d597bba02fe2ef64ade07c7ec273e9926
Thomas Schreiner <iteano.thom@hotmail.com>
2023-01-30 19:05:40 -0300

    feat: add function to show quantity products in the cart

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,14 @@
 import { StyledHeader } from "./style";
 import cart from "../../assets/cart.svg";
+import { useShoopingCartContext } from "../../contexts/shoopingCart";
+import { iShoopingCartProduct } from "../../contexts/shoopingCart/types";
 
 export const Header = () => {
+  const { setIsCartMenuOpen, shoopingCartProducts } = useShoopingCartContext();
+
+  const totalQuantityOfProducts = () =>
+    shoopingCartProducts.reduce((acc: number, act: iShoopingCartProduct) => acc + act.quantity, 0);
+
   return (
     <StyledHeader>
       <nav>
@@ -9,9 +16,9 @@ export const Header = () => {
           <p>MKS</p>
           <p>Sistemas</p>
         </div>
-        <button>
+        <button onClick={() => setIsCartMenuOpen(true)}>
           <img src={cart} alt="cart" />
-          <span>0</span>
+          <span>{totalQuantityOfProducts()}</span>
         </button>
       </nav>
     </StyledHeader>
