1b0fc7f6ef36b47916f6d48e49ab53dfa0c70111
Thomas Schreiner <iteano.thom@hotmail.com>
2023-01-30 19:04:14 -0300

    feat: change format value function to context

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,12 +1,13 @@
 import { StyledCard } from "./style";
 import { iCardProps } from "./type";
 import shopping_bag from "../../assets/shopping_bag.svg";
+import { useShoopingCartContext } from "../../contexts/shoopingCart";
+import { useProductsContext } from "../../contexts/products";
 
 export const Card = ({ product }: iCardProps) => {
-  const { name, brand, photo, price, description } = product;
-
-  const formatValue = (value: number) =>
-    value.toLocaleString("pt-BR", { style: "currency", currency: "BRL", minimumFractionDigits: 0 });
+  const { id, name, brand, photo, price, description } = product;
+  const { formatValueToBRL } = useProductsContext();
+  const { addProductToCart } = useShoopingCartContext();
 
   return (
     <StyledCard>
@@ -16,11 +17,11 @@ export const Card = ({ product }: iCardProps) => {
       <div>
         <div>
           <h2>{name}</h2>
-          <span>{formatValue(Number(price))}</span>
+          <span>{formatValueToBRL(Number(price))}</span>
         </div>
         <p>Redesigned from scratch and completely revised.</p>
       </div>
-      <button>
+      <button onClick={() => addProductToCart(id)}>
         <img src={shopping_bag} alt="shopping bag" />
         <p>COMPRAR</p>
       </button>
