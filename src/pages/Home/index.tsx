9e1b5ffb75148c75aa81912f41aa9377ba06b8c7
Thomas Schreiner <iteano.thom@hotmail.com>
2023-01-30 19:04:59 -0300

    feat: use the shooping cart menu

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,14 @@
 import { CardList } from "../../components/CardList";
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
+import { ShoppingCartMenu } from "../../components/ShoppingCartMenu";
 import { StyledMain } from "./style";
 
 export const Home = () => {
   return (
     <>
       <Header />
+      <ShoppingCartMenu />
       <StyledMain>
         <div>
           <CardList />
